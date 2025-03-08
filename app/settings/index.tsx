247e258bb8dfa45e1ad584eb56aa1fb2a66187e8
iggy <fgrcjr@gmail.com>
2025-03-08 14:17:23 +0800

    add jest and settings folder

--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -1,5 +1,4 @@
 import { Text, View } from "react-native";
-import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -10,8 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/settings">Settings</Link>
+      <Text>Settings Page.</Text>
     </View>
   );
 }
